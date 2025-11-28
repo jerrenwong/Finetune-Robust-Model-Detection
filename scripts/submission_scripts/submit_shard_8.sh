631fb71e33d53895e900243811a4fef2e3581eec
Jer Ren Wong <Jer Ren Wong>
2025-11-28 00:29:06 -0500

    Modified finetuning and generation scripts

---
+++ scripts/submission_scripts/submit_shard_8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -o logs/generate_responses_shard_8.log-%j
+#SBATCH -c 20
+#SBATCH --gres=gpu:volta:1
+#SBATCH --time=48:00:00
+
+# Loading Modules
+source /etc/profile
+module load conda/Python-ML-2025b-pytorch
+
+python scripts/generate_responses.py --num_shards 10 --shard_id 8
